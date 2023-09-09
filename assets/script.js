4275bcd1548d9676740b1631c5a12d39f739ef3b
leviathan <jake66martin@hotmail.com>
2023-09-09 15:27:09 -0400

    click event added to all buttons

--- assets/script.js
+++ assets/script.js
@@ -11,8 +11,14 @@ $(function () {
 
 
 
+ 
+
+
   var hour = parseInt(dayjs().format('H'));
-  console.log(hour)
+  
+  var buttons = $("button")
+
+
 
   hourArray = []
 
@@ -85,6 +91,18 @@ $(function () {
   }
 
 
+  
+  
+  for (x = 0; x < buttons.length; x++) {
+
+    buttons.on("click", function () {
+ 
+    
+ 
+    })
+ 
+   }
+ 
 
 
 
@@ -101,11 +119,7 @@ $(function () {
 
 
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -114,6 +128,9 @@ $(function () {
   var today = dayjs().format('dddd, MMMM D[th]');
 
   $("#currentDay").text(today)
+
+
+  
 
 
 });
