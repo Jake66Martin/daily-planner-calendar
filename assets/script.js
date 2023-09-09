ababef538e15e1ea78c708524b148ddd7dbc7635
leviathan <jake66martin@hotmail.com>
2023-09-08 22:23:44 -0400

    adding code to color code the time blocks

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,52 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  
+  
+  
+  var hour = parseInt(dayjs().format('H'));
+  
+
+  hourArray = [
+    hour9 = parseInt($("#9").attr("id")),
+    hour10 = parseInt($("#10").attr("id")),
+    hour11 = parseInt($("#11").attr("id")),
+    hour12 = parseInt($("#12").attr("id")),
+    hour13 = parseInt($("#13").attr("id")),
+    hour14 = parseInt($("#14").attr("id")),
+    hour15 = parseInt($("#15").attr("id")),
+    hour16 = parseInt($("#16").attr("id")),
+    hour17 = parseInt($("#17").attr("id"))
+  ]
+
+  for (i = 0; i < hourArray.length; i++) {
+    
+  }
+
+  console.log(typeof hourArray[0])
+
+  // hourArray.push(hour9.attr("id"))
+  // hourArray.push(hour10.attr("id"))
+  // hourArray.push(hour11.attr("id"))
+  // hourArray.push(hour12.attr("id"))
+  // hourArray.push(hour13.attr("id"))
+  // hourArray.push(hour14.attr("id"))
+  // hourArray.push(hour15.attr("id"))
+  // hourArray.push(hour16.attr("id"))
+  // hourArray.push(hour17.attr("id"))
+
+  
+ 
+  
+
+  
+
+  
+
+
+
+
+ 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -22,4 +67,7 @@ $(function () {
  var today = dayjs().format('dddd, MMMM D[th]');
 
  $("#currentDay").text(today)
+
+
 });
+
